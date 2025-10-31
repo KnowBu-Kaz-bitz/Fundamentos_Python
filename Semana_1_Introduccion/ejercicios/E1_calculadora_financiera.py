9c4957d368d0b80f93081e0ef26e0c352b0bc777
Kaz-bits <cesarponce.csp@outlook.com>
2025-10-31 17:29:56 -0600

    Add exercise descriptions for financial calculator and unit conversion
    Added detailed docstrings outlining requirements and objectives for E1_calculadora_financiera.py and E2_unit_conversion.py. These descriptions guide users on the expected functionality and learning goals for each exercise.

---
+++ Semana_1_Introduccion/ejercicios/E1_calculadora_financiera.py
@@ -0,0 +1,15 @@
+"""
+Este script proporciona al usuario un programa capaz de generar un balance financiera de un mes. 
+Objetivo: acercar al aprendíz al uso y concepto de variables, tipos de datos numéricos y operadores artiméticos
+
+Requisitos:
+
+El programa debe realizar lo siguiente:
+
+- El programa debe dar la bienvenida al usuario
+- Debe solicitar al usuario su ingreso mensual neto
+- Debe solicitar al usuario sus gastos separados (vivienda, comida, transporte, etc.)
+- El script debe obtener un gasto totalizado
+- El script debe calcular el balance final (ingresos - gastos)
+- El script debe imprimir un resumen de los ingresos y gastos en la consola en un formato legible
+"""
---
+++ Semana_1_Introduccion/ejercicios/E2_unit_conversion.py
@@ -0,0 +1,16 @@
+"""
+Refozar el uso de variables, la conversión de tipos de datos con float()
+e int(), y la aplicación de fórmulas matemáticas simples
+
+
+Requisitos:
+
+- El programa debe pedir al usuario que introduzca una distancia en kilómetros
+- Debe calcular y almacenar la equivalencia en millas (usa el factor de conversión):
+    1 km = 0.621371 millas
+- Debe mostrar el resultado en una frase clara, por ejemplo: "10 km equivalen a 6.21 millas"
+- Ampliación: Pida al usuario su temperatura corporal en grados Celsius
+- Calcula la equivalencia en grados Fahrenheit (Formula: (Celsius * 9/5) + 32)
+- Muestra el resultado de la temperatura de forma amigable
+"""
+
